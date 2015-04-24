ca3963a908b3126528e7f407dd02737e53702489
Dylan C. Smith <rh@izo.me>
2015-04-23 17:26:26 -0700

    wip

--- lib/rhizome_builder/auth.rb
+++ lib/rhizome_builder/auth.rb
@@ -1,11 +1,5 @@
 module RhizomeBuilder
   module Auth
-
-    #AUTH_ACTIONS = ["post", "put", "delete", "patch"]
-
-    def post
-    end
-
     def authorize!
       key = ENV["RHIZOME_BUILDER_KEY"]
       puts "key = #{key}"
